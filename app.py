2b20ee599db93798147dafdbbb32016e0705afb0
Ashutosh <ashutoshmattagaj2000@gmail.com>
2026-06-17 11:40:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key="AQ.Ab8RN6InLjCTHLdS9UOMh0g87eBToDhLKb5aq61oUqo43hoqhg")
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key="AQ.Ab8RN6Iw2iydQVUGpe17BtqORp2caZrDU-r9JqONPmObszVU9g")
 st.title("⚡Fast learning plan generator for any topic!")
 
